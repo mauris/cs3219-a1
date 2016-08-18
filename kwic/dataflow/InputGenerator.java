3cacd10c15de01abd2242bfd4d8af0d6a3760b3c
Sam-Mauris Yong <mauris@hotmail.sg>
2016-08-18 21:29:30 +0800

    implementation of input generator

---
+++ kwic/dataflow/InputGenerator.java
@@ -0,0 +1,19 @@
+package kwic.dataflow;
+
+import java.util.Scanner;
+
+class InputGenerator extends GenericGenerator<String> {
+  public InputGenerator(Pipe<String> pipe) {
+    super(pipe);
+  }
+
+  @Override
+  public void run() {
+    Scanner sc = new Scanner(System.in);
+    while (sc.hasNextLine()) {
+      // read input and add into pipe
+      String line = sc.nextLine();
+      this.putIntoOutput(line);
+    }
+  }
+}
