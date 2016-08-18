e3073ecbd5287928c7240228decb7575752ee2c6
Sam-Mauris Yong <mauris@hotmail.sg>
2016-08-18 22:52:57 +0800

    added implementation in KWIC

--- kwic/dataflow/KWIC.java
+++ kwic/dataflow/KWIC.java
@@ -2,6 +2,18 @@ package kwic.dataflow;
 
 public class KWIC {
   public static void main(String[] args) {
-    
+    Pipe<String> pipe1 = new Pipe<String>();
+    Pipe<String> pipe2 = new Pipe<String>();
+    Pipe<String> pipe3 = new Pipe<String>();
+
+    InputGenerator input = new InputGenerator(pipe1);
+    CircularShiftFilter circularShift = new CircularShiftFilter(pipe1, pipe2);
+    AlphabetizeFilter alphabetize = new AlphabetizeFilter(pipe2, pipe3);
+    OutputSink output = new OutputSink(pipe3);
+
+    (new Thread(output)).start();
+    (new Thread(input)).start();
+    (new Thread(circularShift)).start();
+    (new Thread(alphabetize)).start();
   }
 }
