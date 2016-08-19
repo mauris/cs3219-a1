c32d7f0946a488b9621c44fb96b07750ba7bd90a
Sam-Mauris Yong <mauris@hotmail.sg>
2016-08-19 10:35:47 +0800

    added support to build ignore word list using first line

--- kwic/dataflow/CircularShiftFilter.java
+++ kwic/dataflow/CircularShiftFilter.java
@@ -1,14 +1,48 @@
 package kwic.dataflow;
 
 import kwic.utility.WordTokenizer;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Arrays;
 
 class CircularShiftFilter extends GenericFilter<String, String> {
+  private List<String> ignoreWordList;
+
   public CircularShiftFilter(Pipe<String> input, Pipe<String> output) {
     super(input, output);
+    this.ignoreWordList = new LinkedList<String>();
+  }
+
+  public void setIgnoreWordList(List<String> list) {
+    this.ignoreWordList = list;
+  }
+
+  protected boolean isWordIgnored(String word) {
+    for (String ignoredWord : this.ignoreWordList) {
+      if (ignoredWord.equalsIgnoreCase(word)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  protected static String buildShift(int index, String[] words) {
+    String[] result = new String[words.length];
+    for (int j = index; j < words.length + index; ++j) {
+      result[j - index] = words[j % words.length];
+    }
+    return String.join(" ", result);
+  }
+
+  protected void processIgnoreWordsLine(Pipe<String> input) {
+    String ignoreWordsLine = input.nextOrWait();
+    List<String> list = Arrays.asList(ignoreWordsLine.split("\\W+"));
+    this.setIgnoreWordList(list);
   }
 
   @Override
   protected void transformPipes(Pipe<String> input, Pipe<String> output){
+    this.processIgnoreWordsLine(input);
     while (true) {
       String value = input.nextOrWait();
       if (value == null) {
@@ -22,11 +56,10 @@ class CircularShiftFilter extends GenericFilter<String, String> {
   private void processInput(String line, Pipe<String> output) {
     String[] words = WordTokenizer.tokenize(line);
     for (int i = 0; i < words.length; ++i) {
-      String[] result = new String[words.length];
-      for (int j = i; j < words.length + i; ++j) {
-        result[j - i] = words[j % words.length];
+      if (this.isWordIgnored(words[i])) {
+        continue;
       }
-      output.put(String.join(" ", result));
+      output.put(this.buildShift(i, words));
     }
   }
 }
