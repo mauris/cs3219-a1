9ea1401789523e78c14715d61d49f8849c75c7dc
Ck91 <chenzhengkai91@gmail.com>
2016-08-19 15:31:36 +0800

    implementation of adt Output

---
+++ kwic/adt/Output.java
@@ -0,0 +1,13 @@
+package kwic.adt;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Output {
+  public static void print(ArrayList<String> results){
+    Collections.sort(results);
+    for (int i=0;i<results.size();i++){
+      System.out.println(results.get(i));
+    }
+  }
+}
