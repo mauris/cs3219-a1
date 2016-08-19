56c801d22382bf0f7eb427f61d414b42d3f39bdd
Sam-Mauris Yong <mauris@hotmail.sg>
2016-08-19 09:43:49 +0800

    ensure case insensitive comparison in PQ

--- kwic/dataflow/AlphabetizeFilter.java
+++ kwic/dataflow/AlphabetizeFilter.java
@@ -1,5 +1,6 @@
 package kwic.dataflow;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class AlphabetizeFilter extends GenericFilter<String, String> {
@@ -7,9 +8,17 @@ class AlphabetizeFilter extends GenericFilter<String, String> {
     super(input, output);
   }
 
+  private static Comparator<String> createComparator() {
+    return new Comparator<String>() {
+      public int compare(String alpha, String bravo) {
+        return alpha.compareToIgnoreCase(bravo);
+      }
+    };
+  }
+
   @Override
   protected void transformPipes(Pipe<String> input, Pipe<String> output){
-    PriorityQueue<String> list = new PriorityQueue<String>();
+    PriorityQueue<String> list = new PriorityQueue<String>(this.createComparator());
     while (true) {
       String value = input.nextOrWait();
       if (value == null) {
