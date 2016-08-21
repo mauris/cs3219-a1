5aa7387b31da76dba49316fd02089f2a111fd819
Sam-Mauris Yong <mauris@hotmail.sg>
2016-08-21 16:44:11 +0800

    refactor alphabetize comparator in adt

--- kwic/adt/Alphabetize.java
+++ kwic/adt/Alphabetize.java
@@ -2,15 +2,12 @@ package kwic.adt;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
+import kwic.utility.AlphabetizeComparator;
 
 public class Alphabetize {
   public static void process (Storage storage){
     ArrayList<String> sorted = storage.getResults();
-    Collections.sort(sorted, new Comparator<String>() {
-      public int compare(String entry1, String entry2) {
-        return entry1.compareToIgnoreCase(entry2);
-      }});
+    Collections.sort(sorted, new AlphabetizeComparator());
     storage.setFinal(sorted);
   }
 }
