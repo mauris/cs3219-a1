ba9cc37480bb72584cd124037485afe6d53ea235
Sam-Mauris Yong <mauris@hotmail.sg>
2016-08-24 22:22:56 +0800

    synchronisation fix

--- kwic/dataflow/Pipe.java
+++ kwic/dataflow/Pipe.java
@@ -12,19 +12,28 @@ class Pipe<T> {
   private Queue<T> buffer = new LinkedList<T>();
 
   private final Semaphore isAvailable = new Semaphore(0);
+  private final Semaphore bufferSemaphore = new Semaphore(1);
 
-  public synchronized void put(T value) {
-    buffer.offer(value);
-    isAvailable.release();
+  public void put(T value) {
+    try {
+      bufferSemaphore.acquire();
+      buffer.offer(value);
+      isAvailable.release();
+      bufferSemaphore.release();
+    } catch(InterruptedException ex) {
+      System.out.println(ex);
+    }
   }
 
   public T nextOrWait() {
     try {
       isAvailable.acquire();
+      bufferSemaphore.acquire();
       T value = buffer.poll();
+      bufferSemaphore.release();
       return value;
     } catch(InterruptedException ex) {
-
+      System.out.println(ex);
     }
     return null;
   }
